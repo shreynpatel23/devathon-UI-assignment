80068a476c7e29a666630a17386be8d17d9befd5
Shrey Patel <shreynpatel23@gmail.com>
2019-06-14 16:08:20 +0530

    final product

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,103 +1,137 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../services/product.service';
-import { Product, Options } from '../model/product.model';
-import { NgForm } from '@angular/forms';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import {
+    Component,
+    OnInit
+} from '@angular/core';
+import {
+    ProductService
+} from '../services/product.service';
+import {
+    Product,
+    Options
+} from '../model/product.model';
+import {
+    NgForm
+} from '@angular/forms';
+import {
+    THIS_EXPR
+} from '@angular/compiler/src/output/output_ast';
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+    selector: 'app-product-details',
+    templateUrl: './product-details.component.html',
+    styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
 
-  selectedProduct:Product;
-  showProductDetails:boolean = false;
-  products:Product[];
-  addImage:boolean = false;
-  selectedProductImages:string[];
-  showError:boolean = false;
+    selectedProduct: Product;
+    showProductDetails: boolean = false;
+    products: Product[];
+    addImage: boolean = false;
+    selectedProductImages: string[];
+    showError: boolean = false;
+    errorMessage: string;
+    totalInventory = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  totalInventory = [1,2,3,4,5,6,7,8,9,10];
-
-  constructor(private productService:ProductService) {}
-  ngOnInit() {
-    this.productService.getData().subscribe(
-      (data:any) => {
-        this.products= data.products;
-        this.products.map(product => product.selectedImage = product.images[0]);
-      }
-    )
-  }
-
-  onSelectProduct(product:Product){
-    this.selectedProduct = product;
-    this.selectedProductImages = product.images.filter(image => image != this.selectedProduct.selectedImage);
-    this.showProductDetails = !this.showProductDetails;
-  }
-
-  closeSelectProduct(){
-    this.showProductDetails = false;
-    this.selectedProduct = null;
-    if(this.addImage === true){
-      this.addImage = false;
+    constructor(private productService: ProductService) {}
+    ngOnInit() {
+        this.productService.getData().subscribe(
+            (data: any) => {
+                this.products = data.products;
+                this.products.map(product => product.selectedImage = product.images[0]);
+            }
+        )
     }
-  }
 
-  addMoreOptions(){
-    let newOption = new Options('',['']);
-    this.selectedProduct.options.push(newOption);
-  }
-  
-  addOptionValues(id:number){
-    this.selectedProduct.options[id].optionValues.push('');
-  }
+    onSelectProduct(product: Product) {
+        this.selectedProduct = product;
+        this.selectedProductImages = product.images.filter(image => image != this.selectedProduct.selectedImage);
+        this.showProductDetails = !this.showProductDetails;
+    }
 
-  onSelectInventory(number:number){
-    this.selectedProduct.totalInventory = number;
-  }
+    closeSelectProduct() {
+        this.showProductDetails = false;
+        this.selectedProduct = null;
+        if (this.addImage === true) {
+            this.addImage = false;
+        }
+    }
 
-  updateProductDetails(form:NgForm){
-    if(form.value.productTitle === '' || form.value.productTitle.length <0 ||
-       form.value.productPrice === '' || form.value.productPrice.length <0 ||
-       form.value.productDescription === '' || form.value.productDescription.length <0 ||
-       form.value.productOfferPrice === '' || form.value.productOfferPrice.length <0 ||
-       form.value.shippingCost === '' || form.value.shippingCost.length <0){
-        this.showError = true;
-    } else{
-      this.showError =false;
-      this.showProductDetails = false;
-      this.selectedProduct.productTitle = form.value.productTitle;
-      this.selectedProduct.productPrice = form.value.productPrice;
-      this.selectedProduct.productDescription = form.value.productDescription;
-      this.selectedProduct.productOfferPrice = form.value.productOfferPrice;
-      this.selectedProduct.shippingCost = form.value.shippingCost;
+    addMoreOptions() {
+        let newOption = new Options('', ['']);
+        this.selectedProduct.options.push(newOption);
     }
-  }
-  uploadImage(){
-    this.addImage = !this.addImage;
-    if(this.showError === true){
-      this.showError = false;
+
+    addOptionValues(id: number) {
+        this.selectedProduct.options[id].optionValues.push('');
     }
-  }
-  onAddNewImage(form:NgForm){
-    let url = form.value.imageURL;
-    let jpeg = 'jpeg'; 
-    let png = 'png';
-    let tif = 'tif';
-    let gif = 'gif';
-    if(url.includes(jpeg) || url.includes(png) || url.includes(tif) || url.includes(gif)){
-      this.addImage = false;
-      this.selectedProduct.images.push(url);
-      this.selectedProductImages = this.selectedProduct.images.filter(image => image != this.selectedProduct.selectedImage);
-      this.showError = false;
-    } else{
-      this.showError = true;
+
+    onSelectInventory(number: number) {
+        this.selectedProduct.totalInventory = number;
     }
-  }
-  onSelectImage(image:string) {
-    this.selectedProduct.selectedImage = image;
-    this.selectedProductImages = this.selectedProduct.images.filter(image => image != this.selectedProduct.selectedImage);
-  }
+
+    updateProductDetails(form: NgForm) {
+        if (form.value.productTitle === '' || form.value.productTitle.length < 0) {
+            this.showError = true;
+			this.errorMessage = 'PRODUCT TITLE CANNOT BE EMPTY';
+        } else if (form.value.productDescription === '' || form.value.productDescription.length < 0) {
+            this.showError = true;
+            this.errorMessage = 'PRODUCT DESCRIPTION CANNOT BE EMPTY';
+        } else if (form.value.productPrice === null || form.value.productPrice < 0) {
+            this.showError = true;
+            if (form.value.productPrice === null) {
+                this.errorMessage = 'PRODUCT PRICE CANNOT BE EMPTY';
+            } else if (form.value.productPrice < 0) {
+                this.errorMessage = 'PRODUCT PRICE CANNOT BE NEGATIVE';
+            }
+        } else if (form.value.productOfferPrice === null || form.value.productOfferPrice < 0) {
+            this.showError = true;
+            if (form.value.productOfferPrice === null) {
+                this.errorMessage = 'PRODUCT OFFER PRICE CANNOT BE EMPTY';
+            } else if (form.value.productOfferPrice < 0) {
+                this.errorMessage = 'PRODUCT OFFER PRICE CANNOT BE NEGATIVE';
+            }
+        } else if (form.value.shippingCost === null || form.value.shippingCost < 0) {
+            this.showError = true;
+            if (form.value.shippingCost === null) {
+                this.errorMessage = 'PRODUCT SHIPPING COST CANNOT BE EMPTY';
+            } else if (form.value.shippingCost < 0) {
+                this.errorMessage = 'PRODUCT SHIPPING COST CANNOT BE NEGATIVE';
+            }
+        } else {
+            this.showError = false;
+            this.showProductDetails = false;
+            this.selectedProduct.productTitle = form.value.productTitle;
+            this.selectedProduct.productPrice = form.value.productPrice;
+            this.selectedProduct.productDescription = form.value.productDescription;
+            this.selectedProduct.productOfferPrice = form.value.productOfferPrice;
+			this.selectedProduct.shippingCost = form.value.shippingCost;
+			this.selectedProduct.totalInventory = form.value.totalInventory;
+        }
+    }
+    uploadImage() {
+        this.addImage = !this.addImage;
+        if (this.showError === true) {
+            this.showError = false;
+        }
+    }
+    onAddNewImage(form: NgForm) {
+        let url = form.value.imageURL;
+        let jpeg = 'jpeg';
+        let png = 'png';
+        let tif = 'tif';
+        let gif = 'gif';
+        if (url.includes(jpeg) || url.includes(png) || url.includes(tif) || url.includes(gif)) {
+            this.addImage = false;
+            this.selectedProduct.images.push(url);
+            this.selectedProductImages = this.selectedProduct.images.filter(image => image != this.selectedProduct.selectedImage);
+            this.showError = false;
+        } else {
+            this.showError = true;
+        }
+    }
+    onSelectImage(image: string) {
+        this.selectedProduct.selectedImage = image;
+        this.selectedProductImages = this.selectedProduct.images.filter(image => image != this.selectedProduct.selectedImage);
+    }
 
 }
