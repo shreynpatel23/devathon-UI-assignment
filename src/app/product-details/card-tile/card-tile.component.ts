f13dc0915605d5b8918a8cd2275836ffd8347963
Shrey Patel <shreynpatel23@gmail.com>
2019-06-15 17:01:13 +0530

    final product

--- src/app/product-details/card-tile/card-tile.component.ts
+++ src/app/product-details/card-tile/card-tile.component.ts
@@ -1,21 +1,172 @@
-import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
-import { Product } from 'src/app/model/product.model';
+import {
+    Component,
+    OnInit,
+    Input,
+    Output,
+    EventEmitter,
+    ViewChild,
+    ElementRef
+} from '@angular/core';
+import {
+    Product,
+    Options,
+    OptionValue
+} from 'src/app/model/product.model';
+import {
+    NgForm
+} from '@angular/forms';
+import { NgxFileDropEntry, FileSystemDirectoryEntry, FileSystemFileEntry } from 'ngx-file-drop';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
-  selector: 'app-card-tile',
-  templateUrl: './card-tile.component.html',
-  styleUrls: ['./card-tile.component.scss']
+    selector: 'app-card-tile',
+    templateUrl: './card-tile.component.html',
+    styleUrls: ['./card-tile.component.scss']
 })
 export class CardTileComponent implements OnInit {
 
-  constructor() { }
-  @Input() product:Product;
-  @Input() selectedProduct:Product;
-  @Output() onSelectProduct = new EventEmitter<Product>();
-  ngOnInit() {
-  }
-  onEditCard() {
-    this.onSelectProduct.emit(this.product);
-  }
+    constructor(private productService: ProductService) {}
+    @Input() product: Product;
+	@Input() selectedProduct: Product;
+	@Output() onSelectProduct = new EventEmitter<Product>();
+	@Input() selectedProductImages:string[];
+	@ViewChild('card') card: ElementRef;
+	@Input() totalHeight:number;
+	@Input() totalWidth:number;
+
+    showBigDiv: boolean = false;
+    
+    showError: boolean = false;
+    errorMessage: string;
+    showProductDetails: boolean = false;
+	totalInventory = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	files: NgxFileDropEntry[] = [];
+	ngOnInit() {
+		console.log(this.card);
+	}	
+	getCss() {
+		console.log(this.card.nativeElement.getBoundingClientRect());
+		const y = this.card.nativeElement.getBoundingClientRect().left;
+		if(y+250 < this.totalWidth) {
+			return {
+				top:this.card.nativeElement.getBoundingClientRect().top,
+				left:y,
+				transformOrigin: 'top left'
+			}
+		} else if( y+250 > this.totalWidth) {
+			return {
+				top:this.card.nativeElement.getBoundingClientRect().top,
+				right: 0,
+				//left:0,
+				transformOrigin: 'top right'
+			}
+		}
+		
+	}
+    getTop() {
+        return this.card.nativeElement.getBoundingClientRect().top;
+    }
+    getLeft() {
+        return this.card.nativeElement.getBoundingClientRect().left;
+    }
+    onEditCard() {
+		this.showBigDiv = !this.showBigDiv;
+		this.onSelectProduct.emit(this.product);
+    }
+    addMoreOptions() {
+        let newOption = new Options();
+        newOption.optionName = '';
+        newOption.optionValues = [];
+        this.selectedProduct.options.push(newOption);
+    }
+
+    addOptionValues(id: number) {
+        this.selectedProduct.options[id].optionValues.push(new OptionValue());
+    }
+
+    onSelectInventory(number: number) {
+        this.selectedProduct.totalInventory = number;
+    }
+
+    updateProductDetails(form: NgForm) {
+        if (form.value.productTitle === '' || form.value.productTitle.length < 0) {
+            this.showError = true;
+            this.errorMessage = 'PRODUCT TITLE CANNOT BE EMPTY';
+        } else if (form.value.productDescription === '' || form.value.productDescription.length < 0) {
+            this.showError = true;
+            this.errorMessage = 'PRODUCT DESCRIPTION CANNOT BE EMPTY';
+        } else if (form.value.productPrice === null || form.value.productPrice < 0) {
+            this.showError = true;
+            if (form.value.productPrice === null) {
+                this.errorMessage = 'PRODUCT PRICE CANNOT BE EMPTY';
+            } else if (form.value.productPrice < 0) {
+                this.errorMessage = 'PRODUCT PRICE CANNOT BE NEGATIVE';
+            }
+        } else if (form.value.productOfferPrice === null || form.value.productOfferPrice < 0) {
+            this.showError = true;
+            if (form.value.productOfferPrice === null) {
+                this.errorMessage = 'PRODUCT OFFER PRICE CANNOT BE EMPTY';
+            } else if (form.value.productOfferPrice < 0) {
+                this.errorMessage = 'PRODUCT OFFER PRICE CANNOT BE NEGATIVE';
+            }
+        } else if (form.value.shippingCost === null || form.value.shippingCost < 0) {
+            this.showError = true;
+            if (form.value.shippingCost === null) {
+                this.errorMessage = 'PRODUCT SHIPPING COST CANNOT BE EMPTY';
+            } else if (form.value.shippingCost < 0) {
+                this.errorMessage = 'PRODUCT SHIPPING COST CANNOT BE NEGATIVE';
+            }
+        } else {
+			this.showError = false;
+			this.showBigDiv = false;
+            this.selectedProduct.productTitle = form.value.productTitle;
+            this.selectedProduct.productPrice = form.value.productPrice;
+            this.selectedProduct.productDescription = form.value.productDescription;
+            this.selectedProduct.productOfferPrice = form.value.productOfferPrice;
+            this.selectedProduct.shippingCost = form.value.shippingCost;
+            this.selectedProduct.totalInventory = form.value.totalInventory;
+        }
+	}
+
+	getOptionValues(e,i,j) {
+		this.selectedProduct.options[i].optionValues[j]= e.target.value;
+	}
+
+	onSelectImage(image: string) {
+        this.selectedProduct.selectedImage = image;
+        this.selectedProductImages = this.selectedProduct.images.filter(image => image != this.selectedProduct.selectedImage);
+	}
+
+	dropped(files: NgxFileDropEntry[]) {
+		this.files = files;
+		for (const droppedFile of files) {
+	 
+		  // Is it a file?
+		  if (droppedFile.fileEntry.isFile) {
+			const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+        	fileEntry.file((file: File) => {
+				var myReader: FileReader = new FileReader();
+
+				myReader.onloadend = (e) => {
+				let fileContent: string = myReader.result as string;;
+				this.selectedProduct.images.push(fileContent);
+            	this.selectedProductImages = this.selectedProduct.images.filter(image => image != this.selectedProduct.selectedImage);
+				}
+				myReader.readAsDataURL(file);
+			})
+			
+		  } else {
+			// It was a directory (empty directories are added, otherwise only files)
+			const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
+			
+		  }
+		}
+	  }
+	 
+	  public fileOver(event){
+	  }
+	 
+	  public fileLeave(event){
+	  }
 
 }
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -25,17 +25,16 @@ import { NgxFileDropEntry, FileSystemDirectoryEntry, FileSystemFileEntry} from '
 export class ProductDetailsComponent implements OnInit {
 	
     selectedProduct: Product;
-    showProductDetails: boolean = false;
-    products: Product[];
-    addImage: boolean = false;
-    selectedProductImages: string[];
-    showError: boolean = false;
-    errorMessage: string;
-    totalInventory = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-	files: NgxFileDropEntry[] = [];
+	products: Product[];
+	selectedProductImages: string[];
+	totalHeight:number;
+	totalWidth:number;
+	@ViewChild('prodDetails') prodDetails: ElementRef;
+	
 	constructor(private productService: ProductService) {}
     ngOnInit() {
-		
+		this.totalHeight = this.prodDetails.nativeElement.offsetHeight;
+		this.totalWidth =this.prodDetails.nativeElement.offsetWidth;
         this.productService.getData().subscribe(
             (data: any) => {
                 this.products = data.products;
@@ -46,128 +45,9 @@ export class ProductDetailsComponent implements OnInit {
 
     onSelectProduct(product: Product) {
 		this.selectedProduct = product;
-        this.selectedProductImages = product.images.filter(image => image != this.selectedProduct.selectedImage);
-        this.showProductDetails = !this.showProductDetails;
+		this.selectedProductImages = product.images.filter(image => this.selectedProduct && image != this.selectedProduct.selectedImage);
+
     }
 
-    closeSelectProduct() {
-        this.showProductDetails = false;
-        this.selectedProduct = null;
-        if (this.addImage === true) {
-            this.addImage = false;
-        }
-    }
-
-    addMoreOptions() {
-		let newOption = new Options();
-		newOption.optionName = '';
-		newOption.optionValues = [];
-        this.selectedProduct.options.push(newOption);
-    }
-
-    addOptionValues(id: number) {
-        this.selectedProduct.options[id].optionValues.push(new OptionValue());
-    }
-
-    onSelectInventory(number: number) {
-        this.selectedProduct.totalInventory = number;
-    }
-
-    updateProductDetails(form: NgForm) {
-        if (form.value.productTitle === '' || form.value.productTitle.length < 0) {
-            this.showError = true;
-			this.errorMessage = 'PRODUCT TITLE CANNOT BE EMPTY';
-        } else if (form.value.productDescription === '' || form.value.productDescription.length < 0) {
-            this.showError = true;
-            this.errorMessage = 'PRODUCT DESCRIPTION CANNOT BE EMPTY';
-        } else if (form.value.productPrice === null || form.value.productPrice < 0) {
-            this.showError = true;
-            if (form.value.productPrice === null) {
-                this.errorMessage = 'PRODUCT PRICE CANNOT BE EMPTY';
-            } else if (form.value.productPrice < 0) {
-                this.errorMessage = 'PRODUCT PRICE CANNOT BE NEGATIVE';
-            }
-        } else if (form.value.productOfferPrice === null || form.value.productOfferPrice < 0) {
-            this.showError = true;
-            if (form.value.productOfferPrice === null) {
-                this.errorMessage = 'PRODUCT OFFER PRICE CANNOT BE EMPTY';
-            } else if (form.value.productOfferPrice < 0) {
-                this.errorMessage = 'PRODUCT OFFER PRICE CANNOT BE NEGATIVE';
-            }
-        } else if (form.value.shippingCost === null || form.value.shippingCost < 0) {
-            this.showError = true;
-            if (form.value.shippingCost === null) {
-                this.errorMessage = 'PRODUCT SHIPPING COST CANNOT BE EMPTY';
-            } else if (form.value.shippingCost < 0) {
-                this.errorMessage = 'PRODUCT SHIPPING COST CANNOT BE NEGATIVE';
-            }
-        } else {
-            this.showError = false;
-            this.showProductDetails = false;
-            this.selectedProduct.productTitle = form.value.productTitle;
-            this.selectedProduct.productPrice = form.value.productPrice;
-            this.selectedProduct.productDescription = form.value.productDescription;
-            this.selectedProduct.productOfferPrice = form.value.productOfferPrice;
-			this.selectedProduct.shippingCost = form.value.shippingCost;
-			this.selectedProduct.totalInventory = form.value.totalInventory;
-        }
-	}
-	getOptionValues(e,i,j) {
-		this.selectedProduct.options[i].optionValues[j]= e.target.value;
-	}
-    uploadImage() {
-        this.addImage = !this.addImage;
-        if (this.showError === true) {
-            this.showError = false;
-        }
-    }
-    onAddNewImage(form: NgForm) {
-        let url = form.value.imageURL;
-        if (url.includes('jpeg') || url.includes('png') || url.includes('tif') || url.includes('gif')) {
-            this.addImage = false;
-            this.selectedProduct.images.push(url);
-            this.selectedProductImages = this.selectedProduct.images.filter(image => image != this.selectedProduct.selectedImage);
-            this.showError = false;
-        } else {
-			this.showError = true;
-			form.reset();
-        }
-    }
-    onSelectImage(image: string) {
-        this.selectedProduct.selectedImage = image;
-        this.selectedProductImages = this.selectedProduct.images.filter(image => image != this.selectedProduct.selectedImage);
-	}
-
-	dropped(files: NgxFileDropEntry[]) {
-		this.files = files;
-		for (const droppedFile of files) {
-	 
-		  // Is it a file?
-		  if (droppedFile.fileEntry.isFile) {
-			const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
-        	fileEntry.file((file: File) => {
-				var myReader: FileReader = new FileReader();
-
-				myReader.onloadend = (e) => {
-				let fileContent: string = myReader.result as string;;
-				this.selectedProduct.images.push(fileContent);
-            	this.selectedProductImages = this.selectedProduct.images.filter(image => image != this.selectedProduct.selectedImage);
-				}
-				myReader.readAsDataURL(file);
-			})
-			
-		  } else {
-			// It was a directory (empty directories are added, otherwise only files)
-			const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
-			
-		  }
-		}
-	  }
-	 
-	  public fileOver(event){
-	  }
-	 
-	  public fileLeave(event){
-	  }
 
 }
